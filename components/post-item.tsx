ffaddae813d36aa127c94f0410bb6f856a7397ce
Christoffer <nordin.christoffer@gmail.com>
2025-10-10 11:15:06 +0200

    PostItem component implementation.

---
+++ components/post-item.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Item, ItemActions, ItemContent, ItemDescription, ItemHeader, ItemMedia, ItemTitle } from "./ui/item";
+import { Post } from "@/lib/types";
+
+export default function PostItem({ post }: { post: Post }) {
+    return (
+        <article className="flex w-full flex-col gap-6">
+            <Item variant="outline">
+                <ItemHeader>
+                    <ItemMedia>
+                        <Avatar className="size-10">
+                            <AvatarImage src="https://github.com/evilrabbit.png" className="rounded-full" />
+                            <AvatarFallback>ER</AvatarFallback>
+                        </Avatar>
+                    </ItemMedia>
+                    <ItemContent>
+                        <ItemTitle>{post.profile.name}</ItemTitle>
+                    </ItemContent>
+                    <ItemContent className="flex-none">
+                        <ItemDescription>{post.profile.user}</ItemDescription>
+                        <ItemDescription>{post.created}</ItemDescription>
+                    </ItemContent>
+                </ItemHeader>
+                <ItemContent>
+                    <ItemTitle>{post.title}</ItemTitle>
+                    <ItemDescription>{post.content}</ItemDescription>
+                </ItemContent>
+                <ItemActions>
+                </ItemActions>
+            </Item>
+        </article>
+    )
+}
