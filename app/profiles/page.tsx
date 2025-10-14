cfd3b71795c2a5ba65170cc3f2877cc00551085d
Christoffer <nordin.christoffer@gmail.com>
2025-10-14 16:17:39 +0200

    Profiles page structuring.

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,9 +1,14 @@
-import { SearchParams } from "@/lib/types";
+import ContentProfiles from "@/components/content-profiles";
+import { ProfileListParams, SearchParams } from "@/lib/types";
 
-export default async function Home({ searchParams }: { searchParams: SearchParams }) {
-    const { page, sort, filter } = await searchParams;
+export default async function Page({ searchParams }: { searchParams: SearchParams }) {
+    const { page, sort } = await searchParams;
 
+    const params: ProfileListParams = {
+        page: page ? parseInt(page.toString()) : 1,
+        sort: sort ? sort.toString() : undefined,
+    }
     return (
-        <h2>Profiles</h2>
+        <ContentProfiles params={params} />
     );
 }
