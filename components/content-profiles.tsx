8789a2dc72bee72793867c32afa20528a8c8f273
Christoffer <nordin.christoffer@gmail.com>
2025-10-15 14:47:54 +0200

    Adds profiles filter.

--- components/content-profiles.tsx
+++ components/content-profiles.tsx
@@ -3,15 +3,21 @@ import Sidebar from "./sidebar";
 import { ProfileService } from "@/lib/service/profiles";
 import { Suspense } from "react";
 import ProfilesList from "./profiles-list";
+import { ProfilesFilter } from "./profiles-filter";
 
 export default function ContentProfiles({ params }: { params: ProfileListParams }) {
 
     const data = ProfileService().getProfiles(params);
+    const filterData = Promise.all([
+        ProfileService().getAnimals(),
+        ProfileService().getCountries()
+    ]);
 
     return (
         <div className="grid grid-content-home">
             <main className="grid-area-content grid grid-cols-1 content-start ml-2 my-2">
                 <h2>Profiles</h2>
+                <ProfilesFilter data={filterData} />
                 <Suspense fallback={<p>Loading...</p>}>
                     <ProfilesList data={data} />
                 </Suspense>
---
+++ components/profiles-filter.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { useRouter } from "next/navigation";
+import { use } from "react";
+
+export function ProfilesFilter({ data }: { data: Promise<[string[], string[]]> }) {
+    const [animals, countries] = use(data);
+    const ANIMAL_DEFAUT = "all";
+    const COUNTRY_DEFAULT = "all";
+
+    const { replace } = useRouter();
+    const searchParams = useSearchParams();
+    const urlParams = new URLSearchParams(searchParams);
+    //const animal = searchParams.get("animal") ?? ANIMAL_DEFAUT;
+    //const country = searchParams.get("country") ?? COUNTRY_DEFAULT;
+
+    function onAnimalChange(e: string): void {
+        if (e !== ANIMAL_DEFAUT)
+            urlParams.set("sort", e);
+        else
+            urlParams.delete("sort");
+        replace(`/profiles/?${urlParams.toString()}`);
+    }
+
+    function onCountryChange(e: string): void {
+        if (e !== COUNTRY_DEFAULT)
+            urlParams.set("filter", e);
+        else
+            urlParams.delete("filter");
+        replace(`/profiles/?${urlParams.toString()}`);
+    }
+
+    return (
+        <section className="flex flex-row mb-2">
+            <Select onValueChange={(e) => onAnimalChange(e)}>
+                <SelectTrigger className="w-30 cursor-pointer">
+                    <SelectValue placeholder="Animal" />
+                </SelectTrigger>
+                <SelectContent>
+                    {animals.map(animal => (
+                        <SelectItem key={animal.toLowerCase()} value={animal.toLowerCase()}>{animal}</SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
+            <Select onValueChange={(e) => onCountryChange(e)}>
+                <SelectTrigger className="w-30 mx-2 cursor-pointer">
+                    <SelectValue placeholder="Country" />
+                </SelectTrigger>
+                <SelectContent>
+                    {countries.map(country => (
+                        <SelectItem key={country.toLowerCase()} value={country.toLowerCase()}>{country}</SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
+        </section>
+    );
+}
