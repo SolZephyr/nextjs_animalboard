72297606c3281eb8566db0b76ad90dd58e11a11e
Christoffer <nordin.christoffer@gmail.com>
2025-10-15 10:41:40 +0200

    Profile item card with dynamic route link.

---
+++ components/profile-item.tsx
@@ -0,0 +1,27 @@
+import { Profile } from "@/lib/types";
+import { Item, ItemContent, ItemDescription, ItemHeader, ItemMedia, ItemTitle } from "./ui/item";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
+
+export default function ProfileItem({ profile }: { profile: Profile }) {
+    return (
+        <Link href={`/profiles/${profile.id}`}>
+            <article>
+                <Item variant="outline" className="p-4 hover:bg-gray-50">
+                    <ItemHeader>
+                        <ItemMedia>
+                            <Avatar className="size-10">
+                                <AvatarImage src={profile.avatar} className="rounded-full" />
+                                <AvatarFallback>ER</AvatarFallback>
+                            </Avatar>
+                        </ItemMedia>
+                        <ItemContent>
+                            <ItemTitle>{profile.name}</ItemTitle>
+                            <ItemDescription>{profile.animal}</ItemDescription>
+                        </ItemContent>
+                    </ItemHeader>
+                </Item>
+            </article>
+        </Link>
+    );
+}
