b43989d643a3ef2d404a0420d5d617fea0e83d65
Christoffer <nordin.christoffer@gmail.com>
2025-10-21 16:38:14 +0200

    Loader on favourite.

--- components/favourites.tsx
+++ components/favourites.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import { RestService } from "@/lib/service/rest";
-import { Star } from "lucide-react";
+import { LoaderCircle, Star } from "lucide-react";
 import { MouseEvent, useState } from "react";
 
 export default function ProfileFavourite({ profileId, favourites = 0, isFavourite = false }: { profileId: number, favourites?: number, isFavourite?: boolean }) {
 
     const [count, setCount] = useState<number>(favourites);
     const [hasFavourite, setFavourite] = useState<boolean>(isFavourite);
+    const [isLoading, setLoading] = useState<boolean>(false);
     const dataId = profileId;
 
     const handleClick = async (event: MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
+        setLoading(true);
         const data = await RestService().favouriteProfile(dataId);
+        setLoading(false);
         if (data?.status == 200) {
             const result: { current: boolean; count: number; } = data.body;
             setCount(result.count);
@@ -22,7 +25,11 @@ export default function ProfileFavourite({ profileId, favourites = 0, isFavourit
 
     return (
         <div className="inline-flex items-center justify-center gap-2 whitespace-nowrap">
-            {count}<button type="button" onClick={(e) => handleClick(e)}><Star data-star className={`cursor-pointer ${hasFavourite ? "fill-yellow-300 stroke-yellow-500 hover:fill-yellow-100 hover:stroke-yellow-300" : "hover:fill-yellow-300 hover:stroke-yellow-500"}}`} /></button>
+            {count}
+            {isLoading
+                ? <LoaderCircle className="animate-spin" />
+                : <button type="button" onClick={(e) => handleClick(e)}><Star data-star className={`cursor-pointer ${hasFavourite ? "fill-yellow-300 stroke-yellow-500 hover:fill-yellow-100 hover:stroke-yellow-300" : "hover:fill-yellow-300 hover:stroke-yellow-500"}}`} /></button>
+            }
         </div>
     );
 }
