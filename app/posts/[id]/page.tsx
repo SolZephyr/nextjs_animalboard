c0b9e1366674f7b04d4f4522394fa28643e3b589
Christoffer <nordin.christoffer@gmail.com>
2025-10-22 18:22:51 +0200

    Dynamic metadata.

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,7 +1,24 @@
 import ContentMain from "@/components/content-main";
 import PostContent, { PostContentSkeleton } from "@/components/post-content";
+import { PostsService } from "@/lib/service/posts";
 import { DynamicParams } from "@/lib/types";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { Suspense } from "react";
+
+export async function generateMetadata({ params }: { params: DynamicParams }): Promise<Metadata> {
+    const { id } = await params;
+    const postId = id ? Number(id) : -1;
+
+    const post = await PostsService().getPost(postId);
+    if (!post) {
+        return notFound();
+    }
+    return {
+        title: `Post: ${post.title}`,
+        description: `Pet Project: News on ${post.profile?.name}`
+    }
+}
 
 export default async function Page({ params }: { params: DynamicParams }) {
     const { id } = await params;
