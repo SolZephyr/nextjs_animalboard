ab80832b8fbef838639614c563f57bc2bc334cf4
Christoffer <nordin.christoffer@gmail.com>
2025-10-22 20:22:33 +0200

    Header adjustments.

--- components/header-user.tsx
+++ components/header-user.tsx
@@ -5,7 +5,7 @@ import { UserRound } from "lucide-react";
 
 export default function HeaderUser() {
     return (
-        <div className="grid grid-col-1">
+        <div className="grid grid-col-1 ml-4">
             <SignedOut>
                 <SignInButton>
                     <button className="flex flex-row justify-center items-center cursor-pointer hover:underline hover:decoration-solid" title="Login">
--- components/header.tsx
+++ components/header.tsx
@@ -2,20 +2,21 @@ import { Cat } from "lucide-react";
 import SearchBar from "./searchbar";
 import HeaderUser from "./header-user";
 import { Suspense } from "react";
+import Link from "next/link";
 
 export default function Header() {
 
     return (
-        <header className="content-grid fixed top-[0] left-[0] right-[0] z-999 w-full h-(--header-height) bg-white">
-            <section className="flex flex-row items-center justify-between border border-red-500 text-black p-2">
-                <div className="flex flex-row items-center">
-                    <Cat className="w-10 h-10 mr-1" />
-                    <h1 className="hidden md:block text-xl">PET-PROJECT</h1>
-                </div>
+        <header className="content-grid fixed top-[0] left-[0] right-[0] z-999 w-full h-(--header-height) bg-gray-100">
+            <section className="flex flex-row items-center justify-between h-full border border-red-500 text-black p-2">
+                <Link href={`/`} className="flex flex-row items-center cursor-pointer mr-2 hover:stroke-blue-500 hover:text-blue-500">
+                    <Cat className="w-10 h-10 mr-2" />
+                    <h1 className="hidden md:block text-2xl font-bold">PET-PROJECT</h1>
+                </Link>
                 <Suspense>
                     <SearchBar />
                 </Suspense>
-                <HeaderUser />
+                <HeaderUser/>
             </section>
         </header>
     );
